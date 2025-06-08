36a0c48686cd50d16711d2c221c6e167d86715b7
rohit175041 <rohit175041@hmail.com>
2025-06-08 18:49:58 +0530

    using go logger

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,65 +11,52 @@ import (
 	"time"
 
 	"github.com/rohit154041/students-api/internal/config"
-	"github.com/rohit154041/students-api/logger"
+	"github.com/rohit154041/students-api/internal/http/handlers/student"
 )
 
 func main() {
-	// Load config
+	// load config
 	cfg := config.MustLoad()
+	
+	// database setup
 
-	// Custom logger setup
-	myLogger, loggerErr := logger.NewLogger(logger.INFO, cfg.LogPath)
-	if loggerErr != nil {
-		panic(fmt.Sprintf("Failed to create logger: %v", loggerErr))
-	}
-	defer myLogger.Close()
-
-	myLogger.Info("Starting the students API server")
-
-	// Setup router
+	// setup router
 	router := http.NewServeMux()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			myLogger.Warn("Method %s not allowed on /", r.Method)
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		myLogger.Info("Served welcome message to client")
-		w.Write([]byte("welcome to students api"))
-	})
 
-	// Setup server
+	router.HandleFunc("POST /api/students", student.New())
+	// router.HandleFunc("GET /api/students/{id}", student.GetById(storage))
+	// router.HandleFunc("GET /api/students", student.GetList(storage))
+	// setup server
+
 	server := http.Server{
-		Addr:    cfg.HTTPServer.Addr,
+		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
-	// Graceful shutdown handling
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
 	done := make(chan os.Signal, 1)
+
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	// Start server in goroutine
 	go func() {
-		myLogger.Info("Server listening on %s", cfg.HTTPServer.Addr)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			myLogger.Fatal("ListenAndServe error: %v", err)
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
 		}
-		myLogger.Info("Server stopped listening")
 	}()
 
-	// Wait for shutdown signal
 	<-done
-	myLogger.Info("shutting down the server")
 
-	// Graceful shutdown
+	slog.Info("shutting down the server")
+
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
-		myLogger.Error("Error during shutdown: %v", err)
-	} else {
-		myLogger.Info("Server shut down gracefully")
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
+
+	slog.Info("server shutdown successfully")
 
 }
